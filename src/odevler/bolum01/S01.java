d3cfef4ceac358e1ee1742161ef280229b2d1297
volkan istek <volkanistek@mail.com>
2024-01-28 17:45:58 +0100

    init

---
+++ src/odevler/bolum01/S01.java
@@ -0,0 +1,9 @@
+package odevler.bolum01;
+
+public class S01 {
+    public static void main(String[] args) {
+        System.out.println("Welcome to Java");
+        System.out.println("Welcome to Computer Science");
+        System.out.println("Programming is fun");
+    }
+}
---
+++ src/odevler/bolum02/Q01.java
@@ -0,0 +1,7 @@
+package odevler.bolum02;
+
+public class Q01 {
+    public static void main(String[] args) {
+
+    }
+}
