be1c047284d11cd6471efc96613ce7e78a92b057
volkan istek <volkanistek@mail.com>
2024-08-06 20:16:53 +0200

    refresh

--- src/chapter1/ShowSyntaxErrors.java
+++ src/chapter1/ShowSyntaxErrors.java
@@ -1,7 +1,7 @@
 package chapter1;
 
 public class ShowSyntaxErrors {
-  public static main(String[] args) {
-    System.out.println("Welcome to Java);
+  public static void main(String[] args) {
+    System.out.println("Welcome to Java");
   }
 }
